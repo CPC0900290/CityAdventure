c7a0ba77b64dbce33ccf108e04ee2a0713d1e354
CPC0900290 <62605033+CPC0900290@users.noreply.github.com>
2024-04-19 19:45:51 +0800

    Feature: ThirdTaskVC Logic finish Increase FoodForm CoreML model Modify the RecognizerVC for judge the photo

--- CityAdventure/CityAdventure/View/ThirdTask/RecognizerViewController.swift
+++ CityAdventure/CityAdventure/View/ThirdTask/RecognizerViewController.swift
@@ -8,10 +8,16 @@
 import Foundation
 import UIKit
 import AVFoundation
+import CoreML
+import Vision
 
 class RecognizerViewController: UIViewController {
+  var answer: String?
   private let captureSession = AVCaptureSession()
   private let videoOutput = AVCaptureVideoDataOutput()
+  private var input: AVCaptureDeviceInput?
+  private var bufferSize: CGSize = .zero
+  private let videoDataOutputQueue = DispatchQueue(label: "VideoDataOutput", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -20,37 +26,70 @@ class RecognizerViewController: UIViewController {
   
   // MARK: - UI Setup
   private func configureSession() {
-    captureSession.sessionPreset = AVCaptureSession.Preset.medium
     let availableDevices = AVCaptureDevice.DiscoverySession(
       deviceTypes: [.builtInWideAngleCamera],
       mediaType: AVMediaType.video,
       position: .back
-    ).devices
+    ).devices.first
     
+    captureSession.sessionPreset = AVCaptureSession.Preset.medium
+    // Add input data
     do {
-      let input = try AVCaptureDeviceInput(device: availableDevices[0])
-      captureSession.addInput(input)
-      
-      captureSession.addOutput(videoOutput)
-      
-      let previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
-      previewLayer.videoGravity = .resizeAspect
-      previewLayer.frame = self.view.layer.bounds
-      self.view.layer.addSublayer(previewLayer)
-      
-      DispatchQueue.global(qos: .background).async {
-          self.captureSession.startRunning()
+      let input = try AVCaptureDeviceInput(device: availableDevices!)
+      guard captureSession.canAddInput(input) else {
+        print("Could not add video device input to the session")
+        return
       }
+      captureSession.addInput(input)
     } catch {
-      print(error)
+      print("Could not create video device input: \(error)")
       return
+    }
+    
+    // Add output data
+    if captureSession.canAddOutput(videoOutput) {
+      captureSession.addOutput(videoOutput)
+        // Add a video data output
+      videoOutput.alwaysDiscardsLateVideoFrames = true
+      videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_420YpCbCr8BiPlanarFullRange)]
+      videoOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
+    } else {
+      print("Could not add video data output to the session")
+      return
+    }
+    
+    let previewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+    previewLayer.videoGravity = .resizeAspect
+    previewLayer.frame = self.view.layer.bounds
+    self.view.layer.addSublayer(previewLayer)
+    DispatchQueue.global(qos: .background).async {
+      self.captureSession.startRunning()
     }
   }
 }
 
-extension RecognizerViewController: AVCapturePhotoCaptureDelegate {
-  func photoOutput(_ output: AVCapturePhotoOutput,
-                   didCapturePhotoFor resolvedSettings: AVCaptureResolvedPhotoSettings) {
+extension RecognizerViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+  func captureOutput(_ output: AVCaptureOutput,
+                     didOutput sampleBuffer: CMSampleBuffer,
+                     from connection: AVCaptureConnection) {
+    let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
     
+    do {
+      let modelConfiguration = MLModelConfiguration()
+      let model = try FoodFromTaiwan(configuration: modelConfiguration)
+      let input = FoodFromTaiwanInput(image: pixelBuffer!)
+      let output = try model.prediction(input: input)
+      
+      guard let answer = answer,
+            let similarity = output.classLabelProbs[answer]
+      else { return }
+      if similarity > 60 {
+        // TODO 成功畫面
+        self.navigationController?.popToRootViewController(animated: true)
+      }
+      print(output.classLabel)
+    } catch {
+      print(error.localizedDescription)
+    }
   }
 }
